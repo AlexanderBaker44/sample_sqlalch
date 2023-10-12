15d646d6f776e0e202143165542b1cb052ae24da
AlexanderBaker44 <51001780+AlexanderBaker44@users.noreply.github.com>
2023-10-12 19:25:42 -0400

    Add files via upload

--- streamlit_dash.py
+++ streamlit_dash.py
@@ -19,12 +19,35 @@ st.table(df)
 # streamlit_app.py
 
 import streamlit as st
-
+import pandas as pd
+import matplotlib.pyplot as plt
+st.set_option('deprecation.showPyplotGlobalUse', False)
 # Initialize connection.
 conn = st.experimental_connection("postgresql", type="sql")
 
 # Perform query.
-df = conn.query('SELECT * FROM rna LIMIT 10;', ttl="10m")
+df = conn.query('SELECT * FROM rnc_database;', ttl="10m")
+st.header('Sample SQL Dash')
+df[['avg_length','min_length','max_length']] = df[['avg_length','min_length','max_length']].fillna(0)
+metric = st.selectbox('Select metric', options = ['avg_length','min_length','max_length'],index = 0)
 
+c1,c2,c3 = st.columns(3)
+
+with c1:
+    df_g = df.groupby('timestamp').sum()[metric].plot(kind = 'bar')
+    st.pyplot()
+with c2:
+    dfr = df.groupby('alive').count()['id'].plot(kind = 'bar')
+    st.pyplot()
+with c3:
+    dfr2 = df[['alive','avg_length','min_length','max_length']].groupby('alive').sum()[metric].plot(kind = 'bar')
+    st.pyplot()
+
+selections = list(set(df['descr']))
 # Print results.
-st.table(df)
+selected = st.multiselect('Select Value', options = selections)
+if selected == []:
+    st.header('Please Select a Value')
+else:
+    select = df[df['descr'].isin(selected)][['id','timestamp','display_name','project_id','alive']]
+    st.table(select)
